460f8ad6aaba24c321c1ebd34d2354d38efda7dd
Martin Herndl <martin@herndl.org>
2021-11-21 14:52:34 +0100

    Complete string length type specifiers

--- tests/Type/WebMozartAssert/AssertTypeSpecifyingExtensionTest.php
+++ tests/Type/WebMozartAssert/AssertTypeSpecifyingExtensionTest.php
@@ -13,6 +13,7 @@ class AssertTypeSpecifyingExtensionTest extends TypeInferenceTestCase
 	public function dataFileAsserts(): iterable
 	{
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/data.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/string.php');
 	}
 
 	/**
