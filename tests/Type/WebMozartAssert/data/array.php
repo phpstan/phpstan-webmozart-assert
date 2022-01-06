6df6553ae2980f541d033e4254c7018b5576d7e1
Martin Herndl <martin@herndl.org>
2022-01-06 20:07:44 +0100

    Extract remaining tests

--- tests/Type/WebMozartAssert/data/array.php
+++ tests/Type/WebMozartAssert/data/array.php
@@ -17,8 +17,14 @@ class ArrayTest
 	}
 
 	/**
-	 * @param mixed $a
+	 * @param array{foo?: string, bar?: string} $a
 	 */
+	public function keyExists(array $a): void
+	{
+		Assert::keyExists($a, 'foo');
+		\PHPStan\Testing\assertType('array{foo: string, bar?: string}', $a);
+	}
+
 	public function validArrayKey($a, bool $b): void
 	{
 		Assert::validArrayKey($a);
@@ -26,6 +32,34 @@ class ArrayTest
 
 		Assert::validArrayKey($b);
 		\PHPStan\Testing\assertType('*NEVER*', $b);
+	}
+
+	/**
+	 * @param int[] $a
+	 */
+	public function count(array $a): void
+	{
+		Assert::count($a, 1);
+		$a1 = array_pop($a);
+		$a2 = array_pop($a);
+		\PHPStan\Testing\assertType('int', $a1);
+		\PHPStan\Testing\assertType('int|null', $a2);
+	}
+
+	/**
+	 * @param int[] $a
+	 */
+	public function minCount(array $a): void
+	{
+		Assert::minCount($a, 1);
+		$a1 = array_pop($a);
+		\PHPStan\Testing\assertType('int', $a1);
+	}
+
+	public function isList($a): void
+	{
+		Assert::isList($a);
+		\PHPStan\Testing\assertType('array', $a);
 	}
 
 }
