95770f66014cd0798ac3a87d118d8d29442cefd9
Fabien Villepinte <fabien.villepinte@gmail.com>
2022-01-07 08:26:40 +0000

    Support remaining count* assertions

--- tests/Type/WebMozartAssert/data/array.php
+++ tests/Type/WebMozartAssert/data/array.php
@@ -17,6 +17,15 @@ class ArrayTest
 	}
 
 	/**
+	 * @param array{foo?: string, bar?: string} $things
+	 */
+	public function keyExists(array $things): void
+	{
+		Assert::keyExists($things, 'foo');
+		\PHPStan\Testing\assertType('array{foo: string, bar?: string}', $things);
+	}
+
+	/**
 	 * @param mixed $a
 	 */
 	public function validArrayKey($a, bool $b): void
@@ -28,4 +37,61 @@ class ArrayTest
 		\PHPStan\Testing\assertType('*NEVER*', $b);
 	}
 
+	/**
+	 * @param int[] $a
+	 */
+	public function count(array $a): void
+	{
+		Assert::count($a, 1);
+		$b = array_pop($a);
+		$c = array_pop($a);
+		\PHPStan\Testing\assertType('int', $b);
+		\PHPStan\Testing\assertType('int|null', $c);
+	}
+
+	/**
+	 * @param int[] $a
+	 */
+	public function minCount(array $a): void
+	{
+		Assert::minCount($a, 1);
+		$b = array_pop($a);
+		\PHPStan\Testing\assertType('int', $b);
+	}
+
+	/**
+	 * @param int[] $a
+	 */
+	public function maxCount(array $a): void
+	{
+		Assert::maxCount($a, 1);
+		\PHPStan\Testing\assertType('array<int>', $a);
+		Assert::maxCount($a, 0);
+		\PHPStan\Testing\assertType('array{}', $a);
+	}
+
+	/**
+	 * @param int[] $a
+	 */
+	public function countBetween(array $a): void
+	{
+		Assert::countBetween($a, 1, 2);
+		$b = array_pop($a);
+		$c = array_pop($a);
+		$d = array_pop($a);
+		\PHPStan\Testing\assertType('int', $b);
+		\PHPStan\Testing\assertType('int|null', $c);
+		\PHPStan\Testing\assertType('int|null', $d);
+	}
+
+	/**
+	 * @param mixed $a
+	 */
+	public function isList($a): void
+	{
+		Assert::isList($a);
+		\PHPStan\Testing\assertType('array', $a);
+		Assert::allString($a);
+		\PHPStan\Testing\assertType('array<string>', $a);
+	}
 }
