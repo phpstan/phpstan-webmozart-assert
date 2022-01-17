895cbf2c05e482134f75f49b6bf6ea4d49171547
Martin Herndl <martin@herndl.org>
2022-01-17 21:02:22 +0100

    Add more tests

--- tests/Type/WebMozartAssert/data/object.php
+++ tests/Type/WebMozartAssert/data/object.php
@@ -7,28 +7,40 @@ use Webmozart\Assert\Assert;
 class ObjectTest
 {
 
-	public function classExists($a): void
+	public function classExists($a, $b): void
 	{
 		Assert::classExists($a);
 		\PHPStan\Testing\assertType('class-string', $a);
+
+		Assert::nullOrClassExists($b);
+		\PHPStan\Testing\assertType('class-string|null', $b);
 	}
 
-	public function subclassOf($a): void
+	public function subclassOf($a, $b): void
 	{
 		Assert::subclassOf($a, self::class);
 		\PHPStan\Testing\assertType('class-string<PHPStan\Type\WebMozartAssert\ObjectTest>|PHPStan\Type\WebMozartAssert\ObjectTest', $a);
+
+		Assert::nullOrSubclassOf($b, self::class);
+		\PHPStan\Testing\assertType('class-string<PHPStan\Type\WebMozartAssert\ObjectTest>|PHPStan\Type\WebMozartAssert\ObjectTest|null', $b);
 	}
 
-	public function interfaceExists($a): void
+	public function interfaceExists($a, $b): void
 	{
 		Assert::interfaceExists($a);
 		\PHPStan\Testing\assertType('class-string', $a);
+
+		Assert::nullOrInterfaceExists($b);
+		\PHPStan\Testing\assertType('class-string|null', $b);
 	}
 
-	public function implementsInterface($a): void
+	public function implementsInterface($a, $b): void
 	{
 		Assert::implementsInterface($a, ObjectFoo::class);
 		\PHPStan\Testing\assertType('PHPStan\Type\WebMozartAssert\ObjectFoo', $a);
+
+		Assert::nullOrImplementsInterface($b, ObjectFoo::class);
+		\PHPStan\Testing\assertType('PHPStan\Type\WebMozartAssert\ObjectFoo|null', $b);
 	}
 
 	public function propertyExists(object $a): void
