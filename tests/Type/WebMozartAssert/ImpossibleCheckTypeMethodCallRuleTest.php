d9b6be334ff7f01e6e6ce14ca01c2d7db39c352d
Martin Herndl <martin.herndl@icis.com>
2022-02-23 10:49:21 +0100

    Add regression test

--- tests/Type/WebMozartAssert/ImpossibleCheckTypeMethodCallRuleTest.php
+++ tests/Type/WebMozartAssert/ImpossibleCheckTypeMethodCallRuleTest.php
@@ -47,6 +47,11 @@ class ImpossibleCheckTypeMethodCallRuleTest extends RuleTestCase
 		]);
 	}
 
+	public function testBug68(): void
+	{
+		$this->analyse([__DIR__ . '/data/bug-68.php'], []);
+	}
+
 	public function testBug85(): void
 	{
 		$this->analyse([__DIR__ . '/data/bug-85.php'], []);
---
+++ tests/Type/WebMozartAssert/data/bug-68.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebmozartAssertBug68;
+
+use Webmozart\Assert\Assert;
+
+$encryptedValue = "some value";
+$valueParts = explode(':', $encryptedValue);
+
+Assert::count(
+	$valueParts,
+	2,
+	'Encrypted secret parameter was expected to consist of 2 parts separated by a colon'
+);
